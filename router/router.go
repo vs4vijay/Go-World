014549433046a55c77f230e317994e15740afe41
Vizay Soni <vs4vijay@gmail.com>
2020-03-14 18:17:28 +0000

    ok

--- router/router.go
+++ router/router.go
@@ -2,14 +2,13 @@ package router
 
 import (
 	"github.com/go-chi/chi"
-
 	"go-world/app"
 )
 
 func New() *chi.Mux {
 	router := chi.NewRouter()
 
-	router.MethodFunc("GET", "/", app.HandleIndex)
+	router.MethodFunc("GET", "/a", app.HandleIndex)
 
 	return router
 }
